d1be0a8f9cca90c0f33c1cf44437e5d0dc36adc7
svetlanov <166555611+svetlanov@users.noreply.github.com>
2025-02-10 22:31:46 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Привет, мир!<br>";
+
+echo "Hello, World with echo!<br>";
+print "Hello, World with print!<br>";
+
+$days = 288;
+$message = 'Все возвращаются на работу!';
+
+echo 'Всего дней: ' . $days . ' ' . $message . "<br>"; 
+echo "Всего дней: {$days} {$message}";
+?>
